123fe25c01437f52a89c54891288c3a030748bfa
Adityadusange <adityadusange139@gmail.com>
2024-10-05 14:15:15 +0530

    body.js upadted 2nd time

--- src/components/Body.js
+++ src/components/Body.js
@@ -74,7 +74,7 @@ const Body = () => {
             </thead>
             {!loading && (
               <tbody>
-                {employees.map((Employee) => (
+                {e.map((Employee) => (
                   <tr
                     key={Employee.id}
                     className="border-b border-gray-200 text-black hover:bg-blue-50 hover:text-black"
